1ceec366bc299d70ed01afb2ffc2d355ee9722e9
Tanmoy0151 <royanupama782@gmail.com>
2022-11-02 19:41:19 +0530

    jagdisvai conflict keliye redy ho

--- prime.js
+++ prime.js
@@ -1,19 +1,19 @@
 function checkPrime(number) {
     let factors=0;
-    for (let i = 0; i <= numhdhber; i++) {
-    if (number % i == 0) {
+    for (let i = 0; i <= number; i++) {
+    if (number % i === 0) {
         factors++;
     }
         }
 
-        if (factors == 2) {
+        if (factors === 2) {
             return true;
         }
-       gdhggfi
+      
     }
         let answer = checkPrime(13);
-        if (answer==true) {
+        if (answer===true) {
             console.log("Prime Number");
-        }else{fbjhfs
+        }else{fb
             console.log("Not a Prime Number");
-        }dhsbfh
+        }
