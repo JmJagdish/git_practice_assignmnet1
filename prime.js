9b07aa7288f2fef9055e67d714bdac72bd2c1748
Tanmoy0151 <royanupama782@gmail.com>
2022-11-02 18:42:53 +0530

    jagdis bhai tum apna code samhalo

--- prime.js
+++ prime.js
@@ -1,6 +1,6 @@
 function checkPrime(number) {
     let factors=0;
-    for (let i = 0; i <= number; i++) {
+    for (let i = 0; i <= numhdhber; i++) {
     if (number % i == 0) {
         factors++;
     }
@@ -9,11 +9,11 @@ function checkPrime(number) {
         if (factors == 2) {
             return true;
         }
-       
+       gdhggfi
     }
         let answer = checkPrime(13);
         if (answer==true) {
             console.log("Prime Number");
-        }else{
+        }else{fbjhfs
             console.log("Not a Prime Number");
-        }
+        }dhsbfh
