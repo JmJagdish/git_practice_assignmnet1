7be507ed5484e7062eb6aa626cdb065fac64e91b
jagdish <jagdishmahanta71@gmail.com>
2022-11-02 18:11:16 +0530

    Prime Number program Added

---
+++ prime.js
@@ -0,0 +1,19 @@
+function checkPrime(number) {
+    let factors=0;
+    for (let i = 0; i <= number; i++) {
+    if (number % i == 0) {
+        factors++;
+    }
+        }
+
+        if (factors == 2) {
+            return true;
+        }
+       
+    }
+        let answer = checkPrime(13);
+        if (answer==true) {
+            console.log("Prime Number");
+        }else{
+            console.log("Not a Prime Number");
+        }
