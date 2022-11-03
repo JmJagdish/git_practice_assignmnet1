630418b9f665211eeadb7eebfc8acdcebf871752
jagdish <jagdishmahanta71@gmail.com>
2022-11-03 08:24:14 +0530

    Correct code by jagdish

--- prime.js
+++ prime.js
@@ -1,6 +1,6 @@
 function checkPrime(number) {
     let factors=0;
-    for (let i = 0; i <= number; i++) {
+    for (let i = 1; i <= number; i++) {
     if (number % i == 0) {
         factors++;
     }
@@ -9,11 +9,12 @@ function checkPrime(number) {
         if (factors == 2) {
             return true;
         }
+        return false;
        
     }
         let answer = checkPrime(13);
         if (answer==true) {
-            console.log("Prime Number");
+            console.log("This is a Prime Number");
         }else{
             console.log("Not a Prime Number");
         }
